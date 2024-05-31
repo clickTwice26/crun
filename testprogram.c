d62e6d406766b4f3f3912a824d68954ccc519bcf
clickTwice26 <shagatoc@gmail.com>
2024-05-31 18:55:45 +0600

    updated README.md

--- testprogram.c
+++ testprogram.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-
-    printf("Hello World!");
+    printf("This is a test program for Crun");
+    printf("Hello World!\n");
     printf("Square of 2: %lf", sqrt(25));
     return 0;
 }
